a4f371b29861c5b79cffdba9ae615111b588651e
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-03-14 09:46:16 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('human is online')
+});
+
+client.login('ODAxNDYzMTY0Mzg1ODg2MjEw.YAhCvA.f5GxXvfLrGSXjFY_BtU4ecowgiU')
