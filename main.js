416475b66816c36f72874fce85c5e8ca23ccb627
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-03-14 18:42:22 +0800

    ask the bot hello and it will reply randomly :ok_hand:

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '-';
+var Discord = require('discord.js');
+var client = new Discord.Client();
+
+var greetings = ['hey', 'hello', 'yo', 'hi', 'greetings', 'sup', 'wassup', 'hey sexy :smirk:', 'uwu', 'gday', 'wadup', 'bro', 'i hate you', 'you underestimate my power', 'hellohello', 'Im feeling kinda tired sorry', 'youre boring', 'stop texting me', 'you be spamming', 'hiya!', 'goodmorning/night bc idk what time of day it is']
+var greetLength = greetings.length
 
 client.once('ready', () => {
 	console.log('human is ready')
 });
 
 client.on('message', message => {
-	if(!message.content.startsWith(prefix) || message.author.bot) return;
+	if(message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).split(' ');
-	const command = args.shift().toLowerCase();
-
-	if(command === 'ping'){
-		message.channel.send('pong-');
+	// if(message.content.startsWith('hey')){
+	// 	message.channel.send('hey sexy :smirk:');
+	// }
+	for (var i = 0; i < greetLength; i++) {
+    //console.log(greetings[i]);
+    if(message.content.startsWith(greetings[i])){
+			message.channel.send(greetings[Math.floor(Math.random() * greetLength)]);
+		}
 	}
 });
 
