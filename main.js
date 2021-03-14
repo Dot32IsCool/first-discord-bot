421e0c01998590cfcbc1faa9bc232e4d2873bee8
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-03-14 12:33:38 +0800

    bot will play pong
    type -ping and the bot will go pong-

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '-';
 
 client.once('ready', () => {
-	console.log('human is online')
+	console.log('human is ready')
 });
 
-client.login('ODAxNDYzMTY0Mzg1ODg2MjEw.YAhCvA.f5GxXvfLrGSXjFY_BtU4ecowgiU')
+client.on('message', message => {
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(' ');
+	const command = args.shift().toLowerCase();
+
+	if(command === 'ping'){
+		message.channel.send('pong-');
+	}
+});
+
+client.login('ODAxNDYzMTY0Mzg1ODg2MjEw.YAhCvA.UxyutS-e4XW1uadSd74ccp5T8Ds');
+
+// run `node main.js` from terminal to boot
